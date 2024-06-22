a224dc86e9b1077522845537627963f524f2029e
Racha Salhi <racha@agot.ai>
2024-06-22 01:08:23 +0100

    add linear head

--- model.py
+++ model.py
@@ -6,20 +6,26 @@ from torch.nn.functional import softmax
 torch.manual_seed(32)
 
 class TransformerDecoder(nn.Module):
-    def __init__(self, num_embeddings: int, embedding_dim: int, block_size: int, heads: int, head_size: int, *args, **kwargs) -> None:
+    def __init__(self, num_tokens: int, embedding_dim: int, block_size: int, heads: int, head_size: int, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.block_size = block_size
         
-        # Embedding layer
-        self.embedding_table = nn.Embedding(num_embeddings, embedding_dim)
+        # Model layers
+        self.embedding_table = nn.Embedding(num_tokens, embedding_dim)
+        self.positional_encodings_table = nn.Embedding(num_tokens, embedding_dim)
         self.attention_block = MultiHeadAttention(heads, head_size, block_size, embedding_dim)
+        self.linear_head = nn.Linear(embedding_dim, num_tokens)
+        
         self.loss_func = nn.CrossEntropyLoss()
         
     def forward(self, tokens, targets):
-        # tokens is of shape [B, T]
-        # targets shape [B, T]
-        logits = self.embedding_table(tokens)
-        # logits shape [B, T, C], where C = embedding dim
+        # tokens is of shape [B, T], targets shape [B, T]
+        emb_input = self.embedding_table(tokens) # [B, T, emb_d]
+        # Add positional encoding
+        pos_emb = self.positional_encodings_table((torch.arange(self.block_size))) # [T, emb_d]
+        emb_input += pos_emb
+        
+        logits = self.linear_head(emb_input)
         # Apply cross-entropy loss
         loss = self.loss_func(logits, targets)
         return logits, loss
@@ -28,11 +34,12 @@ class TransformerDecoder(nn.Module):
         logits, loss = self.forward(idx, targets)
         
 
-class SelfAttentionBlock:
+class ScaledSelfAttentionHead(nn.Module):
     def __init__(self, head_size: int, block_size: int, emb_d: int) -> None:
         self.head_size = head_size
         self.block_Size = block_size
         
+        self.register_buffer('tril', torch.tril(torch.ones((self.block_size, self.block_size))))
         self.key = nn.Linear(emb_d, self.head_size)
         self.query = nn.Linear(emb_d, self.head_size)
         self.value = nn.Linear(emb_d, self.head_size)
@@ -43,23 +50,22 @@ class SelfAttentionBlock:
         q = self.query(inputs) # [B, T, head_size]
         v = self.value(inputs) # [B, T, head_size]
         
-        weights = q @ k.transpose(-2, -1) # [B, T, head_size] * [B, T, head_Size] -> [B, T, head_size] * [B, head_Size, T] = [B, T, T]
+        weights = q @ k.transpose(-2, -1) * self.head_size**-0.5 # [B, T, head_size] * [B, T, head_Size] -> [B, T, head_size] * [B, head_Size, T] = [B, T, T]
         
-        tril = torch.tril(torch.ones((self.block_size, self.block_size)))
-        weights = weights.masked_fill((tril == 0), float('-inf'))
+        weights = weights.masked_fill((self.tril == 0), float('-inf'))
         weights = softmax(weights, dim=1)
         input_w_past_attention = weights @ v # [B, T, T] * [B, T, head_size] = [B, T, head_size]
         
         return input_w_past_attention
     
     
-class MultiHeadAttention: 
+class MultiHeadAttention(nn.Module): 
     def __init__(self, heads: int, head_size: int, block_size: int, emb_d: int) -> None:
         self.heads = heads
         self.head_size = head_size
         self.block_Size = block_size
         
-        self.self_attention_blocks = [SelfAttentionBlock(head_size, block_size, emb_d) for _ in range(heads)]
+        self.self_attention_blocks = [ScaledSelfAttentionHead(head_size, block_size, emb_d) for _ in range(heads)]
     
     def forward(self, inputs):
         outputs = [attention(inputs) for attention in self.self_attention_blocks]
